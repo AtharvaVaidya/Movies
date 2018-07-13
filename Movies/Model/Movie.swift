41565ced08351dbbac8bbc00c4f0a4b8ff764b97
Atharva Vaidya <atharva.vaidya01@gmail.com>
2018-07-13 14:58:30 +0530

    Added Movie object

---
+++ Movies/Model/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by Atharva Vaidya on 13/07/18.
+//  Copyright © 2018 Atharva vaidya. All rights reserved.
+//
+
+import Foundation
+
+struct Movie
+{
+    let title:          String
+    let overview:       String
+    let posterPath:     String
+    let releaseDate:    String
+}
